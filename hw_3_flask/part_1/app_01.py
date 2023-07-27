2a8d1ca76c2842aa517ef4a0fa10d1888c1cfdf6
DanielFalcony <DanielFalcony@gmail.com>
2023-07-27 18:14:59 +0300

    hw_3 in progress

--- hw_3_flask/part_1/app_01.py
+++ hw_3_flask/part_1/app_01.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from models import db, User
+from hw_3_flask.part_1.models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db.init_app(app)
+
 
 # in terminal, type "flask init-db" for creating db from models
 @app.cli.command('init-db')
 def init_db():
     db.create_all()
     print('DB OK!')
+
+
+@app.route('/')
+def index():
+    return 'hello world'
 
 
 if __name__ == '__main__':
